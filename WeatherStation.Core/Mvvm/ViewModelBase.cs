da7fb4c1bdc57696526c66cb46320d6bc3998eda
Georgy Levchenko <georgy.levchenko@ktkprom.com>
2020-11-23 13:24:41 +0400

    Добавьте файлы проекта.

---
+++ WeatherStation.Core/Mvvm/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace WeatherStation.Core.Mvvm
+{
+    public abstract class ViewModelBase : BindableBase, IDestructible
+    {
+        protected ViewModelBase()
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
