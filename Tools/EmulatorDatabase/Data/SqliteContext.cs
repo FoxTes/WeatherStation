861d0bfeaadc57865a3f638d887128ede836aae2
Georgy Levchenko <55885862+FoxTes@users.noreply.github.com>
2020-12-06 23:40:25 +0400

    Refactoring service database.

--- Tools/EmulatorDatabase/Data/SqliteContext.cs
+++ Tools/EmulatorDatabase/Data/SqliteContext.cs
@@ -5,6 +5,7 @@ namespace EmulatorDatabase.Data
 {
     public class SqliteContext : DbContext
     {
+        // TODO: Поправить путь.
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite(@"Data Source=C:\Users\Georgy\Documents\WeatherStation\BusinessAccess\WeatherStation.BusinessAccess.Sqlite\Database\appdb.db");
 
